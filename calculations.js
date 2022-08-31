abbb6ce79fa0ae5772e975215f77fe3c66ccf50a
roshanm0903 <roshanm0903@gmail.com>
2022-08-31 17:30:10 +0530

    modularization 2

--- calculations.js
+++ calculations.js
@@ -1,5 +1,64 @@
+function generatePorfolio(Funds, Data){
+
+    var FundData = Data;
+
+    // get share of fund for each stock.
+    var securityWise = {};
+    var industryWise = {};
+
+    var UserFunds = Funds[0].map((_, colIndex) => Funds.map(row => row[colIndex])); // transpose
+
+    var portfolioAmount  =  UserFunds[1].reduce((a, b) => a + b, 0);
+
+    // get the single fund name which is to be compared with the portfolio
+    var compFund = UserFunds[0][ UserFunds[2].indexOf(true)]; 
+    
+    // console.log("portfolio Amount "+portfolioAmount );
+    // console.log("comp fund "+compFund );
+    // console.log(FundData);
+
+    for (row in FundData){
+        // multiply the security fractional ammount with the fund amount
+        var Amount = UserFunds[1][ UserFunds[0].indexOf(FundData[row][0])];
+        var secAmount = (FundData[row][3]*Amount);
+        FundData[row].push(secAmount );
+        
+        // console.log("Printing Fund Data");
+        // console.log(FundData);
+
+        //summarise by security wise
+        if ( FundData[row][1] in securityWise ){     // { "security name 1" : [ amount , portfolio_percentage, compfund_shareholding], "security name w" : [ amount , portfolio_percentage, compfund_shareholding],...  }
+            // security already exists, add the amounts
+            securityWise[FundData[row][1]][0] += secAmount; // amount
+            securityWise[FundData[row][1]][1] += (secAmount/portfolioAmount); // as percentage
+            securityWise[FundData[row][1]][2] = checkCompFund(compFund,FundData[row]);
+
+        } else{
+            //security doesnt exists, add the security and the amount
+            securityWise[FundData[row][1]] = [ secAmount , secAmount/portfolioAmount, checkCompFund(compFund,FundData[row]) ] ;
+        }
+
+        //sumarise by industry wise
+        if ( FundData[row][2] in industryWise ){
+            // security already exists, add the amounts
+            industryWise[FundData[row][2]][0] += secAmount;
+            industryWise[FundData[row][2]][1] += secAmount/portfolioAmount;
+        } else{
+            //security doesnt exists, add the security and the amount
+            industryWise[FundData[row][2]] = [ secAmount, secAmount/portfolioAmount];
+        }
+
+    }
+    console.log("printing sdcuritywise");
+    console.log(securityWise);
+
+    return [securityWise,industryWise,compFund];
+
+}
+
+
 function dotProduct(vectors){
-            
+    
     var dot_sum = 0;
     var a_sum = 0;
     var b_sum = 0;
@@ -22,7 +81,6 @@ function dotProduct(vectors){
     // vectors = getSortedCommonSecurity(vectors);
 
     return  [Math.round(value ), vectors ];
-
 
 }
 
@@ -52,57 +110,43 @@ function getSortedData(dict){
 
 }
 
-function generatePorfolio(Funds, Data){
-
-    var FundData = Data;
-
-    // get share of fund for each stock.
-    var securityWise = {};
-    var industryWise = {};
-
-    var UserFunds = Funds[0].map((_, colIndex) => Funds.map(row => row[colIndex])); // transpose
-
-    var portfolioAmount  =  UserFunds[1].reduce((a, b) => a + b, 0);
-
-    // get the single fund name which is to be compared with the portfolio
-    var compFund = UserFunds[0][ UserFunds[2].indexOf(true)]; 
+function getSortedOverlapData(a,b){
+    if (a[1] === b[1]) {
+        return 0;
+    }
+    else {
+        return (a[1] > b[1]) ? -1 : 1;
+    }
     
-    // console.log("portfolio Amount "+portfolioAmount );
-    console.log("comp fund "+compFund );
-    console.log(FundData);
+}
 
-    for (row in FundData){
-        // multiply the security fractional ammount with the fund amount
-        var Amount = UserFunds[1][ UserFunds[0].indexOf(FundData[row][0])];
-        var secAmount = (FundData[row][3]*Amount);
-        FundData[row].push(secAmount );
+function getSortedCommonSecurity(dict){
+    var items = Object.keys(dict).map(function(key) {
+          return [key, dict[key][0],dict[key][1],dict[key][2]];
+        });
 
-        //sumarise by security wise
-        if ( FundData[row][1] in securityWise ){     // { "security name 1" : [ amount , portfolio_percentage, compfund_shareholding], "security name w" : [ amount , portfolio_percentage, compfund_shareholding],...  }
-            // security already exists, add the amounts
-            securityWise[FundData[row][1]][0] += secAmount; // amount
-            securityWise[FundData[row][1]][1] += (secAmount/portfolioAmount); // as percentage
-            securityWise[FundData[row][1]][2] = checkCompFund(compFund,FundData[row]);
+    items.sort(function(first, second) {
+        return second[3] - first[3];
+    });
+    
+    return items;
+    
+}
 
-        } else{
-            //security doesnt exists, add the security and the amount
-            securityWise[FundData[row][1]] = [ secAmount , secAmount/portfolioAmount, checkCompFund(compFund,FundData[row]) ] ;
+function transpose(arr){
+    var arr_t = arr[0].map((_, colIndex) => arr.map(row => row[colIndex])); // transpose
+    return arr_t
+}
+
+function getCombinations(Funds){
+    var combiArray = transpose(Funds)[0];
+
+    var results = [];
+        for (var i = 0; i < combiArray.length - 1; i++) {
+            for (var j = i + 1; j < combiArray.length; j++) {
+                results.push([combiArray[i],combiArray[j]]);
+            }
         }
-
-        //sumarise by industry wise
-        if ( FundData[row][2] in industryWise ){
-            // security already exists, add the amounts
-            industryWise[FundData[row][2]][0] += secAmount;
-            industryWise[FundData[row][2]][1] += secAmount/portfolioAmount;
-        } else{
-            //security doesnt exists, add the security and the amount
-            industryWise[FundData[row][2]] = [ secAmount, secAmount/portfolioAmount];
-        }
+    return results;
 
     }
-    // console.log(FundData);
-    console.log(securityWise);
-
-    return [securityWise,industryWise,compFund];
-
-}
---
+++ plot.js
@@ -0,0 +1,134 @@
+function plot(securityWise,industryWise){
+    document.getElementById("plots").innerHTML = ' <canvas id="myChart_sec" style="width:100%;"></canvas> \
+            <canvas id="myChart_ind" style="width:100%;"></canvas>';
+
+    // plot overview graphs
+
+    var limit = 30;
+    var data_sec = getSortedData(securityWise);  //  data_sec = [   [secuty 1, secury 2, ... ], [amt1, amt2,...],  [per1, per2,... ] ]
+    var myChart_sec = new Chart("myChart_sec", {
+            type: "bar",
+            data: {labels: data_sec[0].slice(0, limit),
+                datasets: [{ label: 'Stockwise Distribution',
+                            data:data_sec[1].slice(0, limit)}]
+            },
+            options: {
+                    responsive: true,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Stock wise Distribution"
+                        }
+                    }
+                }
+
+            });
+
+    var data_ind = getSortedData(industryWise);
+    var myChart_ind = new Chart("myChart_ind", {
+            type: "bar",
+            data: {labels: data_ind[0].slice(0, limit),
+                datasets: [{ label: 'Industrywise Distribution', data:data_ind[1].slice(0, limit) }]
+            },
+            options: {
+                    responsive: true,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Industry wise Distribution"
+                        }
+                    }
+                }
+
+            });
+
+
+
+
+}
+
+function plotSecuritesOverlap(securityWise){
+    document.getElementById("plots").innerHTML = ' <canvas id="myChart_sec_ovelap" width=100%  height=1000"></canvas>';
+
+    // plot overview graphs
+    var vectors = getSortedCommonSecurity(securityWise);
+    vectors = transpose(vectors);
+    console.log("printing final vectors");
+    console.log(vectors);
+
+    var limit = 50;
+    const config = new Chart("myChart_sec_ovelap",{
+                type: 'horizontalBar',
+                data: {labels: vectors[0],
+                    datasets: [
+                        { label:"Holding in Portfolio", 
+                           data:vectors[2],
+                           backgroundColor:  "#ff9999"
+                        },
+                        { label:"Holding in Selected Fund", 
+                           data:vectors[3],
+                           backgroundColor:"#99c2ff"
+                        }
+                        
+                ]
+                },
+                options: {
+                    indexAxis: 'y',
+                    elements: {
+                    bar: {
+                        borderWidth: 2,
+                    }
+                    },
+                    responsive: true,
+                    plugins: {
+                    legend: {
+                        position: 'right',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Chart.js Horizontal Bar Chart'
+                    }
+                    }
+                },
+                });
+
+
+
+}
+
+function plotPairWiseOverlap(combinations){
+    document.getElementById("plots").innerHTML = '<canvas id="myChart_overlap" style="width:100%;max-width:700px"></canvas>';
+
+    // console.log("printing combinations");
+    // console.log(combinations);
+    // plot overview graphs
+    var combinations = transpose(combinations);
+
+    const config = new Chart("myChart_overlap",{
+        type: 'horizontalBar',
+        data: {labels: combinations[0],
+            datasets: [{ label:"Pair wise overlap", data:combinations[1]}]
+        },
+        options: {
+            indexAxis: 'y',
+            elements: {
+            bar: {
+                borderWidth: 2,
+            }
+            },
+            responsive: true,
+            plugins: {
+            legend: {
+                position: 'right',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Horizontal Bar Chart'
+            }
+            }
+        },
+        });
+
+
+
+}
